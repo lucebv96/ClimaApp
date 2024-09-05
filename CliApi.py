56bc2ab4bde82fb24aa160e8ad57ddfc5610bf6e
Lucero Bareto <barretolucero.lb@gmail.com>
2024-09-05 16:52:28 -0400

    Actu del Jueves

--- CliApi.py
+++ CliApi.py
@@ -1,4 +1,5 @@
 #Este es mi Archivo para el challenge
 #esta es mi primera actualizacion
 #Esta es la primera actualizacion de mi rama1 
-#Segunda Actualizacion  de mi rama1
+#Segunda Actualizacion  de mi 
+#Hola esta es una actualizacion del dia siguiente
